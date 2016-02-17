10443ec23e64e9c403923b23412ad3a63744fb8a
tmfulton <TimFulton@Tims-MacBook-Pro-2.local>
2016-02-17 10:20:00 -0500

    Add Xposed Test Module
    Hopefully add the Xposed test module

---
+++ XposedTest2/app/src/main/java/com/xposedTest/Tutorial.java
@@ -0,0 +1,16 @@
+package com.xposedTest;
+
+/**
+ * Created by TimFulton on 2/10/16.
+ */
+
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class Tutorial implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
